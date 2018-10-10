1bafbb5b8e9f35bbd660048b6cf69359c51aaf18
Poonguzhali Puratchi <Poonguzhali Puratchi@30.8.76.62>
2018-10-10 13:33:00 +0200

    Sample Springboot application

---
+++ sampleSpringboot/src/main/java/com/cheyyar/springboot/SampleSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.cheyyar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringbootApplication.class, args);
+	}
+}
